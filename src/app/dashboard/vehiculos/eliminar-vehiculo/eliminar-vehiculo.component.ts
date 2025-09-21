b86de7736518557745e99ce97a8bd42a006f29ab
oscar.carrillo <oscar.carrillo@ejercito.mil.co>
2025-09-20 21:03:49 -0500

    Vehiculo: V1: Eliminar

--- src/app/dashboard/vehiculos/eliminar-vehiculo/eliminar-vehiculo.component.ts
+++ src/app/dashboard/vehiculos/eliminar-vehiculo/eliminar-vehiculo.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { VehiculosService } from '../../../service/vehiculos-service';
 
 @Component({
   selector: 'app-eliminar-vehiculo',
   templateUrl: './eliminar-vehiculo.component.html',
   styleUrls: ['./eliminar-vehiculo.component.css']
 })
-export class EliminarVehiculoComponent {}
+export class EliminarVehiculoComponent {
+  id: string = '';
+  mensaje: string = '';
 
+  constructor(private vehiculosService: VehiculosService) {}
+
+  eliminarVehiculo() {
+    if (this.id && /^\d+$/.test(this.id)) {
+      const idNum = Number(this.id);
+      this.vehiculosService.eliminarVehiculo(idNum).subscribe({
+        next: () => this.mensaje = 'Vehículo eliminado exitosamente.',
+        error: () => this.mensaje = 'Error al eliminar el vehículo.'
+      });
+    } else {
+      this.mensaje = 'Por favor ingrese un ID válido.';
+    }
+  }
+}
